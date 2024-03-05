2d5596d36216f46d351da85a78066f95db47f7f1
juhee77 <bora2233@naver.com>
2024-03-05 22:51:58 +0900

    init : graphql 프로젝트 실습 설정

---
+++ s2403-graphql-apllo/src/main/java/com/lahezy/s2403graphqlapllo/S2403GraphqlAplloApplication.java
@@ -0,0 +1,13 @@
+package com.lahezy.s2403graphqlapllo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S2403GraphqlAplloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S2403GraphqlAplloApplication.class, args);
+	}
+
+}
