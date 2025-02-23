35aa5caf6f402214edc3cfa7cc701d0b1f04fc86
lahezy <jouhi3322@gmail.com>
2025-02-23 23:46:58 +0900

    feat : jooq init project

---
+++ jooq-study/src/main/java/com/jooq_study/jooq_study/JooqStudyApplication.java
@@ -0,0 +1,13 @@
+package com.jooq_study.jooq_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JooqStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JooqStudyApplication.class, args);
+	}
+
+}
---
+++ jooq-study/src/test/java/com/jooq_study/jooq_study/JooqStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jooq_study.jooq_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JooqStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
